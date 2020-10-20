03c9939f552cb9b36337dc9486be007746dfed6d
AW <alexander.werther@alfalaval.com>
2020-10-20 09:04:03 +0200

    Initial commit

---
+++ src/Readme.txt
@@ -0,0 +1,10 @@
+    * Get things started by opening the Package Manager console and run: Update-Database.
+    * Fix bug that you can´t update Genre to handle space between characters
+    * Verify that ResetRating functionality works, update sql query if necessary, do not change to Entity Framework
+    * Verify that ResetGenre stored procedure functionality works, update stored procedure if necessary, do not change to Entity Framework
+    * Clean up code, remove unused usings
+    * Verify that unit tests are correct with correct functionality
+    * Verify that mapping between models is correct
+    * Use connectionstring in appsettings.json in SpecialOperationsMovieService
+    * Set startup page to Movies / Index page
+    * Make sure unit tests are green and solution builds and all functionality works.
---
+++ src/TestCore/Services/SpecialOperationsMovieService.cs
@@ -0,0 +1,38 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace TestCore.Services
+{
+    public class SpecialOperationsMovieService : ISpecialOperationsMovieService
+    {
+        private readonly string _connectionString = "Server=(localdb)\\mssqllocaldb;Database=MovieContext-bc;Trusted_Connection=True;";
+
+        public async Task ResetRating(int id)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                var query = "UPDATE Movie SET Rating = '' WHERE ID = 1";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    connection.Open();
+                    await command.ExecuteNonQueryAsync();
+                }
+            }
+        }
+
+        public async Task ResetGenre(int id)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                using (var command = new SqlCommand("ResetGenre", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@id", id);
+                    connection.Open();
+                    await command.ExecuteNonQueryAsync();
+                }
+            }
+        }
+    }
+}
